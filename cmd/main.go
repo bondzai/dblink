858747ebcb4344665c4dc09e017a2a888192e90e
James Bond <introbond.dev@gmail.com>
2024-06-02 11:27:47 +0700

    WIP: Remove mock data

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"log"
-	"strconv"
-
 	"github.com/bondzai/dblink/internal/interfaces/handlers"
 	"github.com/bondzai/dblink/internal/interfaces/repositories"
 	"github.com/bondzai/dblink/internal/services"
@@ -14,32 +9,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 )
-
-var ctx = context.Background()
-
-func init() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-
-	mdb := repositories.NewMockRepo()
-
-	users := mdb.MockUsers()
-
-	for _, user := range users {
-		data, err := json.Marshal(user)
-		if err != nil {
-			log.Fatalf("Could not marshal user: %v", err)
-		}
-
-		err = rdb.Set(ctx, strconv.Itoa(user.UserID), data, 0).Err()
-		if err != nil {
-			log.Fatalf("Could not set user data in Redis: %v", err)
-		}
-	}
-
-	log.Println("Successfully seeded Redis with initial user data.")
-}
 
 func main() {
 	app := fiber.New()

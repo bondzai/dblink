23ca42b58219e21165993f130c45585d1ca33f70
James Bond <introbond.dev@gmail.com>
2024-06-03 13:57:46 +0700

    WIP: Test statble with wasm

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,115 @@
 package main
 
 import (
-	"github.com/bondzai/dblink/internal/interfaces/handlers"
-	"github.com/bondzai/dblink/internal/interfaces/repositories"
-	"github.com/bondzai/dblink/internal/services"
+	"sync"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
+	"github.com/google/uuid"
+)
+
+type DriverDTO struct {
+	Location     DriverLocation     `json:"location"`
+	LoginSession DriverLoginSession `json:"loginSession"`
+	Type         DriverType         `json:"type"`
+	Job          *uuid.UUID         `json:"job"`
+}
+
+type DriverLocation struct {
+	Lat  string `json:"lat"`
+	Long string `json:"long"`
+}
+
+type DriverLoginSession struct {
+	DeviceId string `json:"deviceId"`
+}
+
+type DriverType struct {
+	CompanyApproveStatus int       `json:"companyApproveStatus"`
+	JobAcceptStatus      *int      `json:"jobAcceptStatus"`
+	IsInternalCompany    bool      `json:"isInternalCompany"`
+	VehicleTypeID        uuid.UUID `json:"vehicleTypeId"`
+}
+
+var (
+	drivers = make(map[string]DriverDTO)
+	clients = make(map[string]map[*websocket.Conn]bool)
+	lock    = &sync.Mutex{}
+	mutex   = &sync.Mutex{}
 )
 
 func main() {
 	app := fiber.New()
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		driverID := c.Params("id")
 
-	app.Use("/ws", func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
+		lock.Lock()
+		if clients[driverID] == nil {
+			clients[driverID] = make(map[*websocket.Conn]bool)
 		}
-		return fiber.ErrUpgradeRequired
-	})
+		clients[driverID][c] = true
+		lock.Unlock()
 
-	redisRepo := repositories.NewRedisRepository(rdb)
-	userService := services.NewUserService(redisRepo)
-	wsHandler := handlers.NewWebSocketHandler(userService)
+		defer func() {
+			lock.Lock()
+			delete(clients[driverID], c)
+			if len(clients[driverID]) == 0 {
+				delete(clients, driverID)
+			}
+			lock.Unlock()
+			c.Close()
+		}()
 
-	app.Get("/ws/read/:id", websocket.New(wsHandler.ReadUser))
-	app.Get("/ws/update/:id", websocket.New(wsHandler.UpdateUser))
+		for {
+			var updateData map[string]interface{}
+			if err := c.ReadJSON(&updateData); err != nil {
+				return
+			}
+
+			processUpdate(driverID, updateData)
+		}
+	}))
 
 	app.Listen(":8080")
+}
+
+func processUpdate(driverID string, updateData map[string]interface{}) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	driver, exists := drivers[driverID]
+	if !exists {
+		driver = DriverDTO{}
+	}
+
+	if loc, ok := updateData["location"].(map[string]interface{}); ok {
+		if lat, ok := loc["lat"].(string); ok {
+			driver.Location.Lat = lat
+		}
+		if long, ok := loc["long"].(string); ok {
+			driver.Location.Long = long
+		}
+	}
+
+	if session, ok := updateData["loginSession"].(map[string]interface{}); ok {
+		if deviceId, ok := session["deviceId"].(string); ok {
+			driver.LoginSession.DeviceId = deviceId
+		}
+	}
+
+	drivers[driverID] = driver
+	broadcastLocation(driverID, driver)
+}
+
+func broadcastLocation(driverID string, driver DriverDTO) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	for client := range clients[driverID] {
+		if err := client.WriteJSON(driver); err != nil {
+			client.Close()
+			delete(clients[driverID], client)
+		}
+	}
 }
